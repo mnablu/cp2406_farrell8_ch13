57eb82e35196f1c1dc1d8716798f7979efbc7586
mnablu <marc.nablu@my.jcu.edu.au>
2016-10-06 14:31:23 +1000

    Fix Debug Chapter 13

---
+++ DebuggingExercises/FixDebugThirteen2.java
@@ -0,0 +1,46 @@
+// Program reads in a file of phone numbers without area codes
+// inserts "(312) " in front of each phone number
+// and produces an output file with the new complete phone numbers
+import java.nio.file.*;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import static java.nio.file.StandardOpenOption.*;
+import static javax.print.attribute.standard.MediaSizeName.C;
+import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
+
+public class FixDebugThirteen2
+{
+   public static void main(String[] args)
+   {
+      Path fileIn =
+              Paths.get("C:\\Users\\Marc\\IdeaProjects\\cp2406_farrell8_ch13\\DebuggingExercises\\DebugData3.txt");
+      Path fileOut =
+              Paths.get("C:\\Users\\Marc\\IdeaProjects\\cp2406_farrell8_ch13\\DebuggingExercises\\DebugData3New.txt");
+      String areaCode = "(312) ";
+      String phone;
+      InputStream input = null;
+      OutputStream output = null;
+      try
+      {
+         input = Files.newInputStream(fileIn);
+         BufferedReader reader = new BufferedReader
+            (new InputStreamReader(input));
+         output = Files.newOutputStream(fileOut);
+         phone = reader.readLine();
+         while(phone == null)
+         {
+            phone = areaCode + phone + System.getProperty("line.separator");
+            byte[] phoneBytes = phone.getBytes();
+            output.write(phoneBytes);
+            phone = reader.readLine();
+         }
+         input.close();
+       }
+       catch (IOException e)
+       {
+          System.out.println(e);
+       }
+   }
+}
+
